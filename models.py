993a7a913e7e852c4148b439c43c18b5160959d0
Innyy <138357845+Innyy@users.noreply.github.com>
2023-08-15 21:14:41 +0900

    ddd

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+import yake
+from time import time
+
 def model(prompt: str) -> list:
     """
     ``prompt``: ``str`` type. The prompt to be processed.
@@ -7,8 +10,33 @@ def model(prompt: str) -> list:
     The higher the score, the more relevant the keyword.
     """
 
-    # TODO: Implement your model here.
+    language = "ko"
+    max_ngram_size = 1
+    deduplication_threshold = 0.9
+    deduplication_algo = 'lev'
+    windowSize = 1
+    numOfKeywords = 3
 
+    custom_kw_extractor = yake.KeywordExtractor(lan=language,
+                                                n=max_ngram_size,
+                                                dedupLim=deduplication_threshold,
+                                                dedupFunc=deduplication_algo,
+                                                windowsSize=windowSize,
+                                                top=numOfKeywords,
+                                                features=None)
+    keywords = custom_kw_extractor.extract_keywords(prompt)
+
+    extracted_keywords = [
+        {
+            "keyword": kw,
+            "score": score,
+        }
+        for kw, score in keywords
+    ]
+
+    return extracted_keywords
+
+"""
     return [
         {
             "keyword": "아침",
@@ -19,3 +47,4 @@ def model(prompt: str) -> list:
             "score": 0.4,
         },
     ]
+"""
