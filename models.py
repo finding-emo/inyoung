bebde576c93eae1bfbd3359bb005f1d04a36a2a5
Hajin_Kim <kimhajin@yonsei.ac.kr>
2023-08-15 23:36:30 +0900

    ⚡️ Run model loading only once

--- models.py
+++ models.py
@@ -1,5 +1,22 @@
 import yake
-from time import time
+
+language = "ko"
+max_ngram_size = 1
+deduplication_threshold = 0.9
+deduplication_algo = "lev"
+windowSize = 1
+numOfKeywords = 3
+
+custom_kw_extractor = yake.KeywordExtractor(
+    lan=language,
+    n=max_ngram_size,
+    dedupLim=deduplication_threshold,
+    dedupFunc=deduplication_algo,
+    windowsSize=windowSize,
+    top=numOfKeywords,
+    features=None,
+)
+
 
 def model(prompt: str) -> list:
     """
@@ -9,21 +26,6 @@ def model(prompt: str) -> list:
     The list of extracted keywords and their scores.
     The higher the score, the more relevant the keyword.
     """
-
-    language = "ko"
-    max_ngram_size = 1
-    deduplication_threshold = 0.9
-    deduplication_algo = 'lev'
-    windowSize = 1
-    numOfKeywords = 3
-
-    custom_kw_extractor = yake.KeywordExtractor(lan=language,
-                                                n=max_ngram_size,
-                                                dedupLim=deduplication_threshold,
-                                                dedupFunc=deduplication_algo,
-                                                windowsSize=windowSize,
-                                                top=numOfKeywords,
-                                                features=None)
     keywords = custom_kw_extractor.extract_keywords(prompt)
 
     extracted_keywords = [
@@ -36,15 +38,3 @@ def model(prompt: str) -> list:
 
     return extracted_keywords
 
-"""
-    return [
-        {
-            "keyword": "아침",
-            "score": 0.6,
-        },
-        {
-            "keyword": "먹다",
-            "score": 0.4,
-        },
-    ]
-"""
